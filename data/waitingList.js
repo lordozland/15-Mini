84cf586792142f68f0438b9654a0f384ac08f5e6
Santiago Palacios <santipalacios2002@gmail.com>
2021-06-08 21:35:40 -0400

    added a bunch of stuff

---
+++ data/waitingList.js
@@ -0,0 +1,40 @@
+const waitingList = [
+    {
+    "customerName": "Hot Mami",
+    "phoneNumber": "1800hotmami",
+    "customerEmail": "hotmami@gmail.com",
+    "customerID": "hotmami"
+    },
+    {
+    "customerName": "Savvy",
+    "phoneNumber": "1234569635",
+    "customerEmail": "savannah@gmail.com",
+    "customerID": "abc123"
+    },
+    {
+    "customerName": "BMO",
+    "phoneNumber": "Beep Beep Boop Bop Beep",
+    "customerEmail": "email@who.what?",
+    "customerID": "okay"
+    },
+    {
+    "customerName": "Marsha",
+    "phoneNumber": "555-5555",
+    "customerEmail": "MarshaMarshaMarsha@oldersister.com",
+    "customerID": "i'mold"
+    },
+    {
+    "customerName": "Danny DeVito's alternate ego",
+    "phoneNumber": "156434421",
+    "customerEmail": "altvito@ymail.com",
+    "customerID": "altvito"
+    },
+    {
+    "customerName": "summer",
+    "phoneNumber": "123-456-7890",
+    "customerEmail": "summer@email.com",
+    "customerID": "11"
+    }
+    ]
+
+    module.exports = waitingList;
